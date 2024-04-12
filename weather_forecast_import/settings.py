ea6d65ad77919b246886d49d9a9e3fb03740fdd0
matban666 <121500633+matban666@users.noreply.github.com>
2024-04-12 22:02:53 +0100

    Hiding my secrets

--- weather_forecast_import/settings.py
+++ weather_forecast_import/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ywvo&_*0g1-%9se_-nc0237^#oqwtf=#@x_%4v(85@w0m&q@*('
+load_dotenv()
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

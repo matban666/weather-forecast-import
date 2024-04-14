da731abdb09a6e7636deb50e42533d596e62fe57
matban666 <121500633+matban666@users.noreply.github.com>
2024-04-14 10:11:17 +0100

    Update database configuration in settings.py

--- weather_forecast_import/settings.py
+++ weather_forecast_import/settings.py
@@ -80,8 +80,8 @@ WSGI_APPLICATION = 'weather_forecast_import.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.getenv('DATABASE_ENGINE', 'django.db.backends.sqlite3'),
+        'NAME': os.getenv('DATABASE_NAME', os.path.join(BASE_DIR, 'db.sqlite3')),
     }
 }
 
